5e7ae0526742f9a7cfa9685b2bf8d82653057ded
Ludovic Evrin <ludovicevrin@gmail.com>
2024-01-29 16:46:50 +0100

    astrophe retirée

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -32,7 +32,7 @@ const Demo = () => {
 
     if(!isGeolocationEnabled){
  
-         return (<div>Geolocation n'est pas activée </div>)
+         return (<div>Y a pas de géolocalisation activée</div>)
         
     }else if (isWithin20MetersResult){
         return (
@@ -42,7 +42,7 @@ const Demo = () => {
             />
         ); 
     }else {
-        return (<div> tu dois être à l'école !</div>)
+        return (<div>Tu es trop loin de MDS !</div>)
     }
 };
 
